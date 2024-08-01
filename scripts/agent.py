be9aa433a9345db17124cb04a170b6614ffcc130
Gandalf <93679900+gandalf@users.noreply.github.com>
2024-08-01 22:44:50 +0000

    struggling with this superclass but it's basically the only thing between me and the agent

--- scripts/agent.py
+++ scripts/agent.py
@@ -1,18 +1,20 @@
+import _thread
 import fetch
 from fetch import Agent as agent
 import match
 import dragon
 from dragon import Dragon as dragon
 
-class Player(agent):
-    #player parser agent
 
-class Game(agent):
-    #game parser agent
 
-class Match(dragon)):
-    #match parser agent
+def player_scraper():
+    #start player
+
+def match_parser():
+    #start match scraper
+
 
 if __name__ == "__main__":
-    #main function
+    #start player parser agent
+    _thread.start_new_thread(agent, ())
 
--- scripts/fetch.py
+++ scripts/fetch.py
@@ -1,113 +1,116 @@
 #Helper functions to fetch data from the Riot Games API
+import threading
 import sqlite3
 import requests
 import json
 import dragon
-from redis import Redis as redis
+from redis import Redis
+import redis
 import time
 
-class Agent(redis):
+class agent(Redis):
+    def __init__(self, *args, **kwargs):
+        self.connect()
+        super().__init__(self.uri)
+        #super().from_url("redis://localhost:6379/0")
+        self.match_type = "ranked"
+        with open("../secrets/api_key", "r") as file:
+            self.api_key = file.read().strip()
+
+    def connect(self):
+        with open("../secrets/howl-fetch", "r") as file:
+            self.uri = file.read()
+            #self = self.from_url(dragonfly_uri)
+            return self
+
     def get(self, key, default=0):
         val = super().get(key)
         if val == None:
             return default
         return val
 
-def redis_connect():
-    with open("../secrets/howl-fetch", "r") as file:
-        dragonfly_uri = file.read()
-        redis_client = Agent.from_url(dragonfly_uri)
-    return redis_client
+    def handle_match(match):
+        print(match)
+        match = json.loads(match)
+        #get the players in the match
+        participants = match["info"]["participants"]
+        self.db.lpush("players", *[participant["puuid"] for participant in participants])
+        self.db.lpush("match_import", match["metadata"]["matchId"])
 
-#API key
-#fetch from secrets directory
-with open("../secrets/api_key", "r") as file:
-    api_key = file.read().strip()
-    print(type(api_key))
-    print(api_key)
-#define ratelimited request
-def request(url, headers, endpoint):
-    return ratelimit(requests.get, url, headers=headers, endpoint=endpoint)
-
-#Get the matches of a player by PUUID
-def get_matches_by_puuid(puuid, match_type="ranked", start=0, count=100):
-    endpoint = "/lol/match/v5/matches/by-puuid/{puuid}/ids"
-    url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start={start}&count={count}"
-    headers = {
-	    "X-Riot-Token": api_key,
-        "type" : match_type
-    }
-    print(endpoint)
-    response = request(url, headers=headers, endpoint=endpoint)
-    if response.status_code == 200:
-	    return response.json()
-    else:
-	    return None
-
-def get_timeline_by_match(match_id):
-    endpoint = "/lol/match/v5/matches/{matchId}/timeline"
-    url = f"https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}/timeline"
-    headers = {
-        "X-Riot-Token": api_key,
-    }
-    response = request(url, headers=headers, endpoint=endpoint)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
-
-#check the other players in each game
-def get_players_by_match(match_id):
-    endpoint = "/lol/match/v5/matches/{matchId}"
-    url = f"https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}"
-    headers = {
-	    "X-Riot-Token": api_key
-    }
-    response = request(url, headers=headers, endpoint=endpoint)
-    print(response)
-    if response.status_code == 200:
-	#parse the response
-        handle_match(response.json())
-    else:
-        assert False
-        return None
-
-#can lpush(key *vals) to add multiple values to a list
-
-def handle_match(match):
-    #get the players in the match
-    participants = match["info"]["participants"]
-    redis_client.lpush("players", *[participant["puuid"] for participant in participants])
-    redis_client.lpush("match_import", match["metadata"]["matchId"])
-
-def handle_player(player):
-    redis_client.lpush("matches", *get_matches_by_puuid(player))
+    def handle_player(self, player):
+        self.lpush("matches", *self.get_matches_by_puuid(player, self.api_key))
     
-def match_import(match_id):
-    pass
+    def match_import(self, match_id):
+        pass
 
-def ratelimit(func, *args, **kwargs):
-      endpoint = kwargs["endpoint"]
-      redis_client = redis_connect()
-      counter_max = redis_client.get(f"cmax_{endpoint}")
-      interval = redis_client.get(f"interval_{endpoint}")
-      counter = redis_client.get(f"counter{endpoint}")
-      redis_client.incr(f"counter{endpoint}")
-      redis_client.expire(f"counter{endpoint}", interval)
-      if counter < counter_max:
-          return func(*args, **kwargs)
-      else: 
-          time.sleep(interval/counter_max)
-          return func(*args, **kwargs)
+    def ratelimit(self, func, *args, **kwargs):
+        endpoint = kwargs.pop("endpoint")
+        counter_max = int(self.get(f"cmax_{endpoint}"))
+        interval = int(self.get(f"interval_{endpoint}"))
+        counter = int(self.get(f"counter{endpoint}"))
+        self.incr(f"counter{endpoint}")
+        self.expire(f"counter{endpoint}", interval)
+        if counter < counter_max:
+            return func(*args, **kwargs)
+        else: 
+            time.sleep(interval/counter_max)
+            return func(*args, **kwargs)
+
+
+    def request(self, url, headers, endpoint):
+        return self.ratelimit(requests.get, url, headers=headers, endpoint=endpoint)
+
+    #Get the matches of a player by PUUID
+    def get_matches_by_puuid(self, puuid, start=0, count=100):
+        endpoint = "MATCHV5"
+        url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start={start}&count={count}"
+        headers = {
+	        "X-Riot-Token": self.api_key,
+            "type" : self.match_type
+        }
+        print(endpoint)
+        response = self.request(url, headers=headers, endpoint=endpoint)
+        if response.status_code == 200:
+	        return response.json()
+        else:
+	        return None
+
+    def get_timeline_by_match(self, match_id):
+        endpoint = "MATCHV5"
+        url = f"https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}/timeline"
+        headers = {
+            "X-Riot-Token": api_key,
+        }
+        response = self.request(url, headers=headers, endpoint=endpoint)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
+    #check the other players in each game
+    def get_players_by_match(match_id):
+        endpoint = "MATCHV5"
+        url = f"https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}"
+        headers = {
+            "X-Riot-Token": api_key
+        }
+        response = self.request(url, headers=headers, endpoint=endpoint)
+        print(response)
+        if response.status_code == 200:
+        #parse the response
+            handle_match(response.json())
+        else:
+            assert False
+            return None
 
 
 if __name__ == "__main__":
-    redis_client = redis_connect()
+    redis_client = agent()
+    redis_client.connect()
     redis_client.lpush("players", "wiFvhmOQlgki5o5IfTifgk8wEYdpf0GE2Dw87vU-CGQjBNL6VwpibC8YUgpWVhq0ki0M-8P30J80UA")
     player = redis_client.rpop("players").decode("utf-8")
-    while player != None: #while there are still players in the database
-        matches = get_matches_by_puuid(player)
-        for match in matches:
-            handle_match(match)
-        assert False
-        #get the next player
+    redis_client.set("test", "hello world")
+    print(redis_client.get("test"))
+    print(redis_client.keys())
+    print(redis_client.get("interval_MATCHV5", 69))
+    redis_client.handle_player(player)
