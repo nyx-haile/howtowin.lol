4f01681b8ad6ca02463119b5cd665417133fedd8
Gandalf <93679900+gandalf@users.noreply.github.com>
2024-08-01 21:19:27 +0000

    endpoints fixing

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -21,72 +21,90 @@ def redis_connect():
 
 #API key
 #fetch from secrets directory
-with open("../secrets/api_key.txt", "r") as file:
-    api_key = file.read()
-
+with open("../secrets/api_key", "r") as file:
+    api_key = file.read().strip()
+    print(type(api_key))
+    print(api_key)
 #define ratelimited request
-def request(url, headers):
-    return ratelimit(requests.get, url, headers=headers)
+def request(url, headers, endpoint):
+    return ratelimit(requests.get, url, headers=headers, endpoint=endpoint)
 
 #Get the matches of a player by PUUID
-def get_matches_by_puuid(puuid, start=0, count=100):
+def get_matches_by_puuid(puuid, match_type="ranked", start=0, count=100):
+    endpoint = "/lol/match/v5/matches/by-puuid/{puuid}/ids"
     url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start={start}&count={count}"
     headers = {
 	    "X-Riot-Token": api_key,
-        "type" : "ranked"
+        "type" : match_type
     }
-    response = request(url, headers=headers)
+    print(endpoint)
+    response = request(url, headers=headers, endpoint=endpoint)
     if response.status_code == 200:
 	    return response.json()
     else:
 	    return None
 
+def get_timeline_by_match(match_id):
+    endpoint = "/lol/match/v5/matches/{matchId}/timeline"
+    url = f"https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}/timeline"
+    headers = {
+        "X-Riot-Token": api_key,
+    }
+    response = request(url, headers=headers, endpoint=endpoint)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
 #check the other players in each game
-def get_players_by_match(match_id, match_type="ranked"):
+def get_players_by_match(match_id):
+    endpoint = "/lol/match/v5/matches/{matchId}"
     url = f"https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}"
     headers = {
-	    "X-Riot-Token": api_key,
-	    "type" : match_type
+	    "X-Riot-Token": api_key
     }
-    response = request(url, headers=headers)
+    response = request(url, headers=headers, endpoint=endpoint)
+    print(response)
     if response.status_code == 200:
 	#parse the response
         handle_match(response.json())
     else:
-    	return None
+        assert False
+        return None
 
+#can lpush(key *vals) to add multiple values to a list
 
+def handle_match(match):
+    #get the players in the match
+    participants = match["info"]["participants"]
+    redis_client.lpush("players", *[participant["puuid"] for participant in participants])
+    redis_client.lpush("match_import", match["metadata"]["matchId"])
 
+def handle_player(player):
+    redis_client.lpush("matches", *get_matches_by_puuid(player))
+    
+def match_import(match_id):
+    pass
 
-#define ratelimit function which makes sure we don't exceed the rate limit of 100 requests per 2 minutes
-def ratelimit(func,*args, **kwargs):
-      #define two lists. sec_rq stores the number of requests in the last second (max 20)
-      #min_rq stores the number of requests in the last 2 minutes (max 100)
-      now = int(time.time())
-      ts = now  % 100
-      sec_rq = redis_client.get(f"sec_rq{ts}")
-      print(sec_rq)
-      print(redis_client.get(f"min_rq"))
-      assert False
-      min_rq = redis_client.get(f"min_rq") % 100
-      time.sleep(max(redis_client.get(f"min_rq{min_rq}") + 120 - now, 0))
-      if sec_rq <= 20:
-         redis_client.incr(f"sec_rq{ts}")
-         redis_client.expire(f"sec_rq{ts}", now + 1)
-         redis_client.incr("min_rq")
-         redis_client.set(f"min_rq{min_rq}", now)
-         #func(*args, **kwargs)
-      else:
-          time.sleep(1)
-          ratelimit(func, *args, **kwargs)
+def ratelimit(func, *args, **kwargs):
+      endpoint = kwargs["endpoint"]
+      redis_client = redis_connect()
+      counter_max = redis_client.get(f"cmax_{endpoint}")
+      interval = redis_client.get(f"interval_{endpoint}")
+      counter = redis_client.get(f"counter{endpoint}")
+      redis_client.incr(f"counter{endpoint}")
+      redis_client.expire(f"counter{endpoint}", interval)
+      if counter < counter_max:
+          return func(*args, **kwargs)
+      else: 
+          time.sleep(interval/counter_max)
+          return func(*args, **kwargs)
+
 
 if __name__ == "__main__":
-    #set up working loop
-    #get first player from player database
     redis_client = redis_connect()
-    redis_client.lpush("players", "jzHYaBATS33X6EKmKPnIfmMViHErtxjhAq_bHeamH1Ov4Q7C-QfzDZzF45QxHftoDzEQnCFSL6Xycg")
+    redis_client.lpush("players", "wiFvhmOQlgki5o5IfTifgk8wEYdpf0GE2Dw87vU-CGQjBNL6VwpibC8YUgpWVhq0ki0M-8P30J80UA")
     player = redis_client.rpop("players").decode("utf-8")
-    print(player)
     while player != None: #while there are still players in the database
         matches = get_matches_by_puuid(player)
         for match in matches:
