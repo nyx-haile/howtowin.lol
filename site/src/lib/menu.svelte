cb3eee3bbcb617ce6a63ecee28523f2d2f2f7f03
Nyx <93679900+alcaeus@users.noreply.github.com>
2024-08-22 18:28:43 +0100

    learning svelte :(

---
+++ site/src/lib/menu.svelte
@@ -0,0 +1,24 @@
+<style>
+    .menu {
+        display: flex;
+        justify-content: space-around;
+        background-color: #f1f1f1;
+        border-radius: 60px;
+        padding: 100px;
+    }
+    a {
+        text-decoration: none;
+        color: black;
+    }
+</style>
+
+<div class="menu">
+    <a href="/">Home</a><br/>
+    <a href="/about">About</a><br/>
+    <a href="/contact">Contact</a><br/>
+</div>
+
+<div class="menuItem">
+</div>
+
+<h1>Connected</h1>
---
+++ site/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Menu from "$lib/menu.svelte"
+    import "$lib/app.css"
+</script>
+
+<div class="page">
+    <div id="sidebar" width="25%">
+        <Menu />
+        <p>Layout</p>
+    </div>
+    <div id="content" width="75%">
+        <slot/>
+    </div>
+</div>
